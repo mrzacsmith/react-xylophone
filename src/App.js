9c893a42a51a36f3d33c0182b42e07055e21b002
Zac Smith <zrs3141592@gmail.com>
2020-02-08 16:11:16 -0700

    map notes to NoteButton component

--- src/App.js
+++ src/App.js
@@ -16,16 +16,9 @@ const App = () => {
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
-        {/* I am placeholder buttons, please create me using a map, and with your own component */}
-        <NoteButton note={notes[0]} />
-        <button onClick={e => new Audio(notes[0].file).play()}>c</button>
-        <button>d1</button>
-        <button>e1</button>
-        <button>f</button>
-        <button>g</button>
-        <button>a</button>
-        <button>b</button>
-        <button>c2</button>
+        {notes.map(note => (
+          <NoteButton note={note} />
+        ))}
       </div>
       {/* <button>Replay</button> */}
       {/* <button>Clear</button> */}
