e6f8f77d3dcc153944645b0bb36217895268f3d0
Zac Smith <zrs3141592@gmail.com>
2020-02-08 16:00:06 -0700

    createcomponent for NoteButton
    Using props, created and rendered component. It does not currently interate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import notes from "./assets/notes.js";
 import "./App.scss";
 
-function App() {
+const NoteButton = props => {
+  console.log("props", props);
+  return (
+    <button onClick={event => new Audio(props.note.file).play()}>
+      {props.note.name}
+    </button>
+  );
+};
+
+const App = () => {
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
+        <NoteButton note={notes[0]} />
         <button onClick={e => new Audio(notes[0].file).play()}>c</button>
         <button>d1</button>
         <button>e1</button>
@@ -21,6 +31,6 @@ function App() {
       {/* <button>Clear</button> */}
     </div>
   );
-}
+};
 
 export default App;
