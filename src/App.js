2a023a8cceca4fcbe2dc3f2f6f0216a72e866209
Zac Smith <zrs3141592@gmail.com>
2020-02-08 17:00:57 -0700

    add styling based on scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import notes from "./assets/notes.js";
 import "./App.scss";
 
 const NoteButton = props => {
   console.log("props", props);
   return (
-    <button onClick={event => new Audio(props.note.file).play()}>
+    <button
+      style={{ height: 200 - 15 * props.index + "px" }}
+      className="note-button"
+      onClick={() => {
+        props.setPlayedNotes([...props.playedNotes, props.note]);
+        new Audio(props.note.file).play();
+      }}
+    >
       {props.note.name}
     </button>
   );
 };
 
 const App = () => {
+  const [playedNotes, setPlayedNotes] = useState([]);
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
-        {notes.map(note => (
-          <NoteButton note={note} />
+        {notes.map((note, index) => (
+          <NoteButton
+            index={index}
+            playedNotes={playedNotes}
+            setPlayedNotes={setPlayedNotes}
+            key={note.name}
+            note={note}
+          />
         ))}
       </div>
+      {JSON.stringify(playedNotes)}
       {/* <button>Replay</button> */}
       {/* <button>Clear</button> */}
     </div>
